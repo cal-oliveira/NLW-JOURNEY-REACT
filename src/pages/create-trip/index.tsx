6b1c54cf9935f9dedcbbe05c435fef015acd9713
Calebe Oliveira <calebe81899026@gmail.com>
2024-07-22 21:23:24 -0300

    create guest input

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,16 +1,21 @@
-import { ArrowRight, Calendar, MapPin } from 'lucide-react'
+import { ArrowRight, Calendar, MapPin, UserRoundPlus } from 'lucide-react'
+import { useState } from 'react';
 
 export function Home(){
+
+    const [guestsInput, setGuestsInput] = useState(true)
+
     return (
-      <div className="bg-pattern h-screen bg-no-repeat bg-center">
-        <div className="flex flex-col items-center justify-center h-screen space-y-10">
-          <div className='flex flex-col items-center'>
-            <img src={'./logo.svg'} alt="logo planner" />
+      <div className="bg-pattern h-screen bg-no-repeat bg-center flex items-center justify-center">
+        <div className="max-w-3xl w-full px-6 text-center space-y-10">
+          <div className="flex flex-col items-center">
+            <img src={"./logo.svg"} alt="logo planner" />
             <p className="text-zinc-300 text-lg">
               Convide seus amigos e planeje sua próxima viagem!
             </p>
           </div>
 
+          <div className='space-y-4'>
           <div className="h-16 flex items-center bg-zinc-900 rounded-xl shadow-shape px-4 gap-3">
             <div className="flex items-center gap-2 flex-1">
               <MapPin className="text-zinc-400 size-5" />
@@ -34,6 +39,23 @@ export function Home(){
             <button className="bg-lime-300 text-lime-950 flex items-center gap-2 px-5 py-2 rounded-lg hover:bg-lime-700">
               Continuar <ArrowRight className="size-5" />{" "}
             </button>
+          </div>
+
+          {guestsInput && (
+            <div className="h-16 flex items-center bg-zinc-900 rounded-xl shadow-shape px-4 gap-3">
+              <div className="flex items-center gap-2 flex-1">
+                <UserRoundPlus className="text-zinc-400 size-5" />
+                <input
+                  className="bg-transparent text-white outline-none placeholder-zinc-400 flex-1"
+                  type="text"
+                  placeholder="Para onde você vai?"
+                />
+              </div>
+              <button className="bg-lime-300 text-lime-950 flex items-center gap-2 px-5 py-2 rounded-lg hover:bg-lime-700">
+                Confirmar viagem <ArrowRight className="size-5" />{" "}
+              </button>
+            </div>
+          )}
           </div>
 
           <p className="text-zinc-500 text-center text-sm">
